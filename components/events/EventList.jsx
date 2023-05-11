c9c404577208249a8afde5e3fe595f179c347940
Caolan <caolanhamilton75@gmail.com>
2023-05-11 15:38:19 +0100

    add styling and loading

--- components/events/EventList.jsx
+++ components/events/EventList.jsx
@@ -1,7 +1,8 @@
 import EventCard from "./EventCard";
+import styles from "./EventList.module.css";
 export default function EventList({ events }) {
   return (
-    <div className='event_list'>
+    <div className={styles.event_list}>
       {events.map((event) => {
         return <EventCard event={event} key={event.id} />;
       })}
--- components/events/EventSearch.jsx
+++ components/events/EventSearch.jsx
@@ -1,50 +1,57 @@
 import { useRef } from "react";
+import styles from "./EventSearch.module.css";
+import Link from "next/link";
+import { GrPowerReset } from "react-icons/gr";
+import { IoSearchCircleSharp } from "react-icons/io5";
 
 function EventsSearch(props) {
-    const yearInputRef = useRef();
-    const monthInputRef = useRef();
-  
-    function submitHandler(event) {
-      event.preventDefault();
-  
-      const selectedYear = yearInputRef.current.value;
-      const selectedMonth = monthInputRef.current.value;
-  
-      props.onSearch(selectedMonth, selectedYear);    
-    }
-  
-    return (
-      <form className="form" onSubmit={submitHandler}>
-        <div className="controls">
-          <div className="control">
-            <label htmlFor='year'>Year</label>
-            <select id='year' ref={yearInputRef}>
-              <option value='2021'>2021</option>
-              <option value='2022'>2022</option>
-              <option value='2023'>2023</option>
-            </select>
-          </div>
-          <div className="control">
-            <label htmlFor='month'>Month</label>
-            <select id='month' ref={monthInputRef}>
-              <option value='January'>January</option>
-              <option value='February'>February</option>
-              <option value='March'>March</option>
-              <option value='April'>April</option>
-              <option value='May'>May</option>
-              <option value='June'>June</option>
-              <option value='July'>July</option>
-              <option value='August'>August</option>
-              <option value='September'>September</option>
-              <option value='October'>October</option>
-              <option value='November'>November</option>
-              <option value='December'>December</option>
-            </select>
-          </div>
-        </div>
-        <button>Find Events</button>
-      </form>
-    );
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedMonth, selectedYear);
   }
-  
-  export default EventsSearch;
+
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <div className={styles.controls}>
+        <Link href={`/events`} className={styles.all_events}>
+          <GrPowerReset className={styles.all_events_icon} />
+        </Link>
+        <div className={styles.control}>
+          <select id="year" ref={yearInputRef}>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+          </select>
+        </div>
+        <div className={styles.control}>
+          <select id="month" ref={monthInputRef}>
+            <option value="January">January</option>
+            <option value="February">February</option>
+            <option value="March">March</option>
+            <option value="April">April</option>
+            <option value="May">May</option>
+            <option value="June">June</option>
+            <option value="July">July</option>
+            <option value="August">August</option>
+            <option value="September">September</option>
+            <option value="October">October</option>
+            <option value="November">November</option>
+            <option value="December">December</option>
+          </select>
+        </div>
+        <button className={styles.go_btn}>
+          <IoSearchCircleSharp className={styles.go_btn_icon} />
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default EventsSearch;
