6e622a4c6ed90d8e56cf6eed84c6eed9bdf7ba81
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-05-11 16:06:38 +0100

    small change to EventList component

--- components/events/EventList.jsx
+++ components/events/EventList.jsx
@@ -2,7 +2,7 @@ import EventCard from "./EventCard";
 export default function EventList({ events }) {
   return (
     <div className='event_list'>
-      {events.map((event) => {
+      {events?.map((event) => {
         return <EventCard event={event} key={event.id} />;
       })}
     </div>
