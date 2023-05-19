6b9257979e16cb10769b704f96dfd83ee0a67251
aliciabarczak <alicia.barczak@uk.globallogic.com>
2023-05-19 19:09:41 +0100

    add env ver credentials

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    mongoDB_username: "DevopsPG", 
+    mongoDB_password: "dpg2023",
+    mongoDB_cluster: "INSTER", 
+    mongoDB_database: "INSERT",
+  }
 }
 
 module.exports = nextConfig
